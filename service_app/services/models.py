683d62386c4c20346c253ee4925ea4fc9f2b62b6
EvgeniyaAlekseevna <zhenya.sudniczina@yandex.ru>
2024-01-29 22:34:39 +0300

    9 - DB Индексы

--- service_app/services/models.py
+++ service_app/services/models.py
@@ -54,7 +54,15 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
-    comment = models.CharField(max_length=255, null=True)
+    comment = models.CharField(max_length=255, default='', null=True, db_index=True)
+
+    field_a = models.CharField(max_length=255, default='')
+    field_b = models.CharField(max_length=255, default='')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['field_a', 'field_b'])
+        ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
